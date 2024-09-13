2725c838f6489e094a1c28eff0df204192309e22
MarianoMaldonado-dev <cyberwargamesproductions@gmail.com>
2024-09-13 14:32:19 -0300

    misclick/crud/create/UPLOAD[Create spring project and upload all files into repository on misclick branch]

---
+++ practice-experience/src/main/java/com/eam_project/app/practice_experience/PlayingWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.eam_project.app.practice_experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayingWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ practice-experience/src/test/java/com/eam_project/app/practice_experience/PlayingWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam_project.app.practice_experience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
